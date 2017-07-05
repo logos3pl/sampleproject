0523023b788ab035439251641c01a355fe414b51
giman gim <gmgim@imex.co.kr>
2017-07-05 16:04:51 +0900

    수정 삭제 기능 추가.

--- src/main/java/com/example/demo/SampleDAO.java
+++ src/main/java/com/example/demo/SampleDAO.java
@@ -1,11 +1,15 @@
 package com.example.demo;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+
 
 @Repository
 public class SampleDAO {
@@ -15,7 +19,16 @@ public class SampleDAO {
 	
 	public List<Map<String, Object>> selectBoradList(){
 		String sql = "SELECT no, writer, subject, content, hit, regdate FROM board;";
-		String args = "";
 		return template.queryForList(sql);
+	}
+	
+	public int updateUpvoteBoard(int no){
+		String sql = "update board set hit = hit+1 where no = ?;";
+		return template.update(sql, no);
+	}
+	
+	public int  updateBoard(Map<String, Object> map){
+		String sql = "update board set subject = ?, content = ? where no = ?;";
+		return template.update(sql, map.get("subject"), map.get("content"), map.get("no"));
 	}
 }
--- src/main/java/com/example/demo/VueMainController.java
+++ src/main/java/com/example/demo/VueMainController.java
@@ -1,10 +1,16 @@
 package com.example.demo;
 
+
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.junit.runners.Parameterized.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +26,65 @@ public class VueMainController {
 		List<Map<String, Object>> mapList =  sampleDAO.selectBoradList();
 		System.out.println("size : "+mapList.size());
 		return mapList;
+	}
+	
+	@RequestMapping("/upvoteBoard/{no}")
+	@ResponseBody
+	public Object upvoteBoard(@PathVariable int no, @RequestParam Map<String, Object> ModelMap) {
+		System.out.println("no >>> "+no);
+		System.out.println("no >>> "+ModelMap.get("no"));
+		System.out.println("writer >>> "+ModelMap.get("writer"));
+		System.out.println("subject >>> "+ModelMap.get("subject"));
+		System.out.println("content >>> "+ModelMap.get("content"));
+		System.out.println("hit >>> "+ModelMap.get("hit"));
+		System.out.println("regdate >>> "+ModelMap.get("regdate"));
+		
+		
+		int result = sampleDAO.updateUpvoteBoard(no);
+		
+		Map<String,Object> map = new HashMap<String, Object>();
+		map.put("ret", result);
+		return map;
+	}
+	
+	@RequestMapping("/upvoteBoard2/{no}")
+	@ResponseBody
+	public Object upvoteBoard2(@PathVariable int no, @RequestBody Map<String, Object> ModelMap) {
+		System.out.println("no >>> "+no);
+		System.out.println("no >>> "+ModelMap.get("no"));
+		System.out.println("writer >>> "+ModelMap.get("writer"));
+		System.out.println("subject >>> "+ModelMap.get("subject"));
+		System.out.println("content >>> "+ModelMap.get("content"));
+		System.out.println("hit >>> "+ModelMap.get("hit"));
+		System.out.println("regdate >>> "+ModelMap.get("regdate"));
+		
+		
+		int result = sampleDAO.updateUpvoteBoard(no);
+		
+		Map<String,Object> map = new HashMap<String, Object>();
+		map.put("ret", result);
+		return map;
+	}
+	
+	@RequestMapping("/deleteBoard/{no}")
+	@ResponseBody
+	public Object deleteBoard(@PathVariable int no) {
+		System.out.println("no >>> "+no);
+		// 실제로 삭제하지 않고 update만 한다.
+		int result = sampleDAO.updateUpvoteBoard(no);
+		Map<String,Object> map = new HashMap<String, Object>();
+		map.put("ret", result);
+		return map;
+	}
+	
+	@RequestMapping("/updateBoard/{no}")
+	@ResponseBody
+	public Object updateBoard(@PathVariable int no, @RequestBody Map<String, Object> ModelMap) {
+		
+		// 실제로 삭제하지 않고 update만 한다.
+		int result = sampleDAO.updateBoard(ModelMap);
+		Map<String,Object> map = new HashMap<String, Object>();
+		map.put("ret", result);
+		return map;
 	}
 }
